512b71bec6c7b1b5c12967d971db76a41255877c
Lloyd Polley <lloydpolley@googlemail.com>
2020-05-07 00:22:08 +0100

    dark theme added

--- js/click.js
+++ js/click.js
@@ -3,12 +3,12 @@
 
   var countryDescription = 'Where in the world is a React app which fetches data from the restcountries.eu/ API. ';
   var rockDescription = 'This is a React app of the game rock paper scissors in which you play against a computer opponent. The score is recorded per session';
-  var tracksDescription = 'Soundport is a React app with redux for state managament. The files are stored on a Firebase database which enables the user to upload music and the data to be persistant. The idea behind this app was to create a music streaming service with user uploads';
+  var tracksDescription = 'Soundr is a React app with redux for state managament. The files are stored on a Firebase database which enables the user to upload music and the data to be persistant. The idea behind this app was to create a music streaming service with user uploads';
 
 
   var countriesObj = { title: 'Where in the world?', desc: countryDescription, link: 'https://lwp-country-app.netlify.app/', image: './files/imgs/maps.jpg', github: 'https://github.com/LloydPolley/Rest-Countries-API' };
   var rockObj = { title: 'Rock Paper Scissors', desc: rockDescription, link: 'https://lwp-rock-paper-scissors.netlify.app/', image: './files/imgs/rock.jpg', github: 'https://github.com/LloydPolley/Rock-Paper-Scissors' };
-  var tracksObj = { title: 'Soundport', desc: tracksDescription, link: 'https://music-store-project-61ddd.firebaseapp.com/', image: './files/imgs/music.jpg', github: 'https://github.com/LloydPolley/Music-store' };
+  var tracksObj = { title: 'Soundr', desc: tracksDescription, link: 'https://music-store-project-61ddd.firebaseapp.com/', image: './files/imgs/pink.jpg', github: 'https://github.com/LloydPolley/Music-store' };
 
   var clickableWork = document.querySelectorAll(".work-piece");
   var closeModalButton = document.querySelectorAll(".close-modal--js")[0];
@@ -20,7 +20,8 @@
   var modalText = modal.querySelectorAll('p')[0];
   var modalLink = modal.querySelectorAll('.webpage')[0];
   var modalGithub = modal.querySelectorAll('.github')[0];
-  console.log(modal)
+
+  var darkModeToggle = document.querySelectorAll('.dark-mode-toggle')[0];
 
   clickableWork.forEach(function (work) {
     work.addEventListener("click", function (e) {
@@ -48,6 +49,11 @@
       return tracksObj;
     }
   }
+
+  darkModeToggle.addEventListener('click', function(){
+    document.querySelectorAll('.dm-toggle')[0].classList.toggle('dark-mode');
+    console.log('run')
+  })
 
   function toggleModal() {
     modal.classList.toggle('toggleModal');
